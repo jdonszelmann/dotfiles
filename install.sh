18277b48c611818b1386ffd18ff0aee2e62293dd
jonay2000 <jonabent@gmail.com>
2019-12-11 13:26:14 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+stow -v -R -t ~ git
+stow -v -R -t ~/.config/sxhkd sxhkd
+stow -v -R -t ~/.config/polybar polybar
+stow -v -R -t ~/.config/autorandr autorandr
+stow -v -R -t ~/.config/nvim nvim
+stow -v -R -t ~/.config/alacritty alacritty
+stow -v -R -t ~/.config/rofi rofi
+stow -v -R -t ~/.config/dunst dunst
+stow -v -R -t ~/.config/wal wal
+stow -v -R -t ~/.config/btops btops
+stow -v -R -t ~ zsh
+sudo stow -v -R -t /etc pacman
+sudo stow -v -R -t ~ x
