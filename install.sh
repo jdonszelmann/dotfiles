5d79167736603e70b3947d4ef06251a28504983e
jonay2000 <jonabent@gmail.com>
2019-12-18 14:11:58 +0100

    fullscreen fixed

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ stow -v -R -t ~/.config/rofi rofi
 stow -v -R -t ~/.config/dunst dunst
 stow -v -R -t ~/.config/wal wal
 stow -v -R -t ~/.config/btops btops
+stow -v -R -t ~/.config/sound sound
 stow -v -R -t ~ zsh
+sudo stow -v -R -t /etc locale
 sudo stow -v -R -t /etc pacman
 sudo stow -v -R -t ~ x
 sudo stow -v -R -t /etc/systemd/system services
